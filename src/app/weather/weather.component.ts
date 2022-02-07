2b6886a4e10dbe81d6dd1698c1e63cf07020edf4
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-02-07 22:37:47 +0700

    fix weather

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 const URL_API = 'https://api.openweathermap.org/data/2.5/weather?q=Hanoi,vie&APPID='
 const KEY = 'aa23e2f239142a14f1b06d3316a74862'
+
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
@@ -9,32 +11,42 @@ const KEY = 'aa23e2f239142a14f1b06d3316a74862'
 export class WeatherComponent implements OnInit {
   weatherShow = false
   button = true
-  WeatherData:any;
-  constructor() { }
+  WeatherData: any;
+
+  constructor() {
+  }
 
   ngOnInit() {
     this.WeatherData = {
-      main : {},
+      main: {},
       isDay: true
     };
     this.getWeatherData();
     console.log(this.WeatherData);
   }
-  getWeatherData(){
+
+  getWeatherData() {
     fetch(URL_API + KEY)
-      .then(response=>response.json())
-      .then(data=>{this.setWeatherData(data);})
+      .then(response => response.json())
+      .then(data => {
+        this.setWeatherData(data);
+      })
   }
+
   change() {
-   this.weatherShow = true
+    this.weatherShow = true
     this.button = false
   }
+
   change2() {
     this.weatherShow = false
     this.button = true
   }
 
-  setWeatherData(data:any){
+  // let data = JSON.parse('{"coord":{"lon":72.85,"lat":19.01},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],"base":"stations","main":{"temp":297.15,"feels_like":297.4,"temp_min":297.15,"temp_max":297.15,"pressure":1013,"humidity":69},"visibility":3500,"wind":{"speed":3.6,"deg":300},"clouds":{"all":20},"dt":1580141589,"sys":{"type":1,"id":9052,"country":"IN","sunrise":1580089441,"sunset":1580129884},"timezone":19800,"id":1275339,"name":"Mumbai","cod":200}');
+  // this.setWeatherData(data);
+
+  setWeatherData(data: any) {
     this.WeatherData = data;
     let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
     this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
